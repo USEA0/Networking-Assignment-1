21b12524d5ada2e8ac04bd37203a10d9658e038d
Boris <boris.au@uoit.net>
2020-02-28 14:11:31 -0500

    Some more ui changes

--- Assignment 1/Assets/Scripts/UIManager.cs
+++ Assignment 1/Assets/Scripts/UIManager.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices;
 
 public class UIManager : MonoBehaviour
 {
+    //const string DLL_NAME = "pluginName";
+    //[DllImport(DLL_NAME)]
+    //private static extern void savePosition(float x, float y, float z);
+
     // Start is called before the first frame update
     void Start()
     {
