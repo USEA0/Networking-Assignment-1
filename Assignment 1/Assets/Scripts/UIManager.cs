fc422f9de9b0146e332ebe5ad445fb66fc49dc48
Boris <boris.au@uoit.net>
2020-02-29 11:58:40 -0500

    Hooked up UI elements to manager

--- Assignment 1/Assets/Scripts/UIManager.cs
+++ Assignment 1/Assets/Scripts/UIManager.cs
@@ -6,19 +6,89 @@ using System.Runtime.InteropServices;
 
 public class UIManager : MonoBehaviour
 {
-    //const string DLL_NAME = "pluginName";
-    //[DllImport(DLL_NAME)]
-    //private static extern void savePosition(float x, float y, float z);
+    public GameObject ConnectScreen;
+    public GameObject Lobby;
+    public GameObject JoinPlayer;
+    public GameObject GameChat;
+    public GameObject ConnectedPlayers;
+
+    public Text LobbyLog;
+    public Text JoinPlayerText;
+    public Text PlayerList;
+    public Text Chat;
+
+    public InputField IpField;
+    public InputField UsernameField;
+    public InputField ChatField;
+
+    public Button JoinButton;
+    public Button AcceptPlayerButton;
+    public Button RejectPlayerButton;
+    public Button LeaveGameButton;
+
+    private bool isConnected = false;
+    private bool inLobby = false;
+    private bool inGame = false;
+    private bool fieldFilled = false;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ConnectScreen.SetActive(true);
+        Lobby.SetActive(false);
+        JoinPlayer.SetActive(false);
+        GameChat.SetActive(false);
+        ConnectedPlayers.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (!isConnected)
+        {
+            if(IpField.text != "" && UsernameField.text != "")
+            {
+                JoinButton.onClick.AddListener(Connect);
+            }
+        }
+        else if(inLobby)
+        {
+            LobbyScreen();
+        }
+        else if(inGame)
+        {
+            ChatScreen();
+        }
         
+    }
+
+    void Connect()
+    {
+        if (!isConnected)
+        {
+            ConnectScreen.SetActive(false);
+            Lobby.SetActive(true);
+            ConnectedPlayers.SetActive(true);
+            isConnected = true;
+            inLobby = true;
+        }
+    }
+
+    void LobbyScreen()
+    {
+        
+    }
+
+    void ChatScreen()
+    {
+        if (ChatField.text != "")
+        {
+            if (Input.GetKeyDown(KeyCode.Return))
+            {
+                string temp = ChatField.text;
+                Debug.Log(temp);
+                ChatField.text = "";
+            }
+        }
     }
 }
